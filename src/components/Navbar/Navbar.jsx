8aece5dd0184e50b7c2d81be5c3d7424bf66f9db
Martin Nagy <martin.laszlo.nagy@gmail.com>
2022-12-12 22:56:25 +0100

    Add hamburger menu icon on small sizes.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import MenuButtonType from "../../constants/MenuButtonType";
+import HamburgerIcon from "../icons/HamburgerIcon";
 import NavbarBigButton from "./NavbarButtons/NavbarBigButton";
 import { navbarItems } from "./navbarItems";
 
@@ -11,20 +12,27 @@ const Navbar = () => {
   }
 
   return (
-    <div className="hidden flex-col w-24 h-screen bg-gray-100 md_to_infinity:flex">
-      <div className="grid w-full self-center gap-4 justify-center">
-        {navbarItems.map((item) => (
-          <NavbarBigButton
-            children={item.icon}
-            text={item.text}
-            className={item.className}
-            selected={item.menuButtonType === selectedMenuItem}
-            onMenuItemClick={onMenuItemClick}
-            menuButtonType={item.menuButtonType}
-          />
-        ))}
+    <>
+      <div className="hidden flex-col w-24 h-screen bg-gray-100 md:flex">
+        <div className="grid w-full self-center gap-4 justify-center">
+          {navbarItems.map((item) => (
+            <NavbarBigButton
+              children={item.icon}
+              text={item.text}
+              className={item.className}
+              selected={item.menuButtonType === selectedMenuItem}
+              onMenuItemClick={onMenuItemClick}
+              menuButtonType={item.menuButtonType}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="m-1 flex flex-row md:hidden">
+        <div className="hover:bg-gray-200 hover:cursor-pointer flex ring-offset-4 w-12 h-12 rounded-full items-center justify-center relative">
+          <HamburgerIcon className="w-6 h-6 ring-offset-4" />
+        </div>
+      </div>
+    </>
   );
 };
 
